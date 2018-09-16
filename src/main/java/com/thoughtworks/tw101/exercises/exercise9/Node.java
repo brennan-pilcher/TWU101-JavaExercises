d38215618168080ccd357c46fa0f1165fada22e4
Brennan Pilcher <brennan.pilcher@gmail.com>
2018-09-16 19:29:10 -0400

    Began binary tree exercise

--- src/main/java/com/thoughtworks/tw101/exercises/exercise9/Node.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise9/Node.java
@@ -3,13 +3,41 @@ package com.thoughtworks.tw101.exercises.exercise9;
 import java.util.List;
 
 public class Node {
-    private String name;
+    public String name;
+    public Node left, right;
 
     public Node(String name) {
         this.name = name;
     }
 
     public void add(String nameOfNewNode) {
+        int comparison = nameOfNewNode.compareToIgnoreCase(name);
+        if (comparison < 0) {
+            if (left != null) {
+                left.add(nameOfNewNode);
+            }
+            else {
+                left = new Node(nameOfNewNode);
+            }
+        }
+        else if (comparison > 0) {
+            if (right != null) {
+                right.add(nameOfNewNode);
+            }
+            else {
+                right = new Node (nameOfNewNode);
+            }
+        }
+        else {
+            throw new RuntimeException("Adding equivalent strings isn't handled.");
+        }
+    }
+
+    public void print () {
+        System.out.println("Node: " + name);
+        System.out.println("L: " + left.name);
+        System.out.println("R: " + right.name);
+        System.out.println("\n");
     }
 
     public List<String> names() {
